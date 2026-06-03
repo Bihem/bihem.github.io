496f1685bcec5101d8afbdb0da961aaf8f142b9d
Bihem <talseumeclothes@gmail.com>
2026-06-03 19:29:27 +0200

    Add Dikenga OS service worker

---
+++ os-sw.js
@@ -0,0 +1,7 @@
+const C='dikenga-os-v1';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;}).catch(()=>caches.match(e.request)));
+});
